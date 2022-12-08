4f9ef2094ae9306cf32228a77e029410961a136c
Elpeaze <elpeaze@gmail.com>
2022-12-08 10:16:57 -0800

    different elements only

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-
-    diff_set = set_1.difference(set_2)
-
-    diff_set = diff_set.symmetric_difference(set_2)
-
-    return diff_set
+    return(set_1 ^ set_2)
 
