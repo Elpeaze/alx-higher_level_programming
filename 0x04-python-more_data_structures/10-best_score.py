041122438e8ec002978363719feb6d2f2d9637df
Elpeaze <elpeaze@gmail.com>
2022-12-08 10:53:34 -0800

    bestscore

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,13 +1,13 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
+    new_dictionary = {}
 
-    max_value = -1
-
-    max_key = None
-
-    for key, value in a_dictionary.items():
-        if value > max_value:
-            max_value = value
-            max_key = key
-
-    return max_key
-
+    aux = 0
+    if a_dictionary:
+        for key in a_dictionary:
+            if a_dictionary[key] > aux:
+                new_dictionary['name'] = key
+                aux = a_dictionary[key]
+        return new_dictionary['name']
+    else:
+        return None
