7a57d717a2cd86b8fb57e3595d2ace565d87f899
Elpeaze <elpeaze@gmail.com>
2023-05-01 11:05:23 +0100

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const argsLength = process.argv.lenght;
+const argsNum = process.argv.slice(2);
 
-if (argsLength === 2) {
+if (argsNum.length === 0) {
   console.log('No argument');
-} else if (argsLength === 3) {
+} else if (argsNum.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
