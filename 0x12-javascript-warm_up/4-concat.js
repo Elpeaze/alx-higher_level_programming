d793812dacf33483a1e2fbffd68151131a9ce68e
Elpeaze <elpeaze@gmail.com>
2023-05-01 14:40:17 +0100

    Create a sentence

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const [argsNum1, argsNum2] = process.argv.slice(2);
 
-if (argsNum1 === 'c' && argsNum2 === 'cool') {
-  console.log('c is cool');
-} else if (argsNum1 === 'c') {
-  console.log('c is undefined');
+if (argsNum1 && argsNum2) {
+  console.log(argsNum1 + ' is ' + argsNum2);
+} else if (argsNum1) {
+  console.log(argsNum1 + ' is undefined');
 } else {
   console.log('undefined is undefined');
 }
