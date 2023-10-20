c89c7d55b4b3046677f37bd78a768a1a2732fde7
Elpeaze <111974145+Elpeaze@users.noreply.github.com>
2023-10-20 11:59:49 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const [argsNum1] = process.argv.slice(2);
-
-if (!argsNum1) {
-  console.log('No argument');
-} else {
-  console.log(argsNum1);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const [argsNum1] = process.argv.slice(2);
-
-if (isNaN(argsNum1)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + parseInt(argsNum1));
-}
