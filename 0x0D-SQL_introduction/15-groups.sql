a3753ba2c5af0ee889d4f75ab8d9fafcac4eccb1
Elpeaze <elpeaze@gmail.com>
2023-11-15 13:13:34 +0100

    Number by score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Lists the number of records with the same score in the table
 
-SELECT score, DISTINCT COUNT(score) AS number FROM second_table ORDER BY score DESC;
+SELECT DISTINCT COUNT(score) AS number FROM second_table ORDER BY score DESC;
