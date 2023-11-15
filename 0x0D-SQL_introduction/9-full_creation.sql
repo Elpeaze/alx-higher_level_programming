62100bb81d9039b2b5682e07d8ccd1b92392887d
Elpeaze <elpeaze@gmail.com>
2023-11-15 12:23:14 +0100

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,7 @@
 -- Creates a full table in the database
 
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
-INSERT INTO second_table (id, name, score) VALUES ([1, 2, 3, 4], ['John', 'Alex', 'Bob', 'George'], [10, 3, 14, 8]);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10)
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3)
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14)
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
