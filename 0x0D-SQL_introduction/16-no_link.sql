689f55ae16fe617cdc868789b277f7654ba7e17e
Elpeaze <elpeaze@gmail.com>
2023-11-16 08:16:27 +0100

    Say my name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,6 @@
 -- Lists all records of the table of the database
 
-SELECT score, name FROM second_table WHERE name <> NULL ORDER BY score DESC;
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
