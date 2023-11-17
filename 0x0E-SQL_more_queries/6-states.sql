200392f70597e82bf8d97ae1238709585b1b5a90
Elpeaze <elpeaze@gmail.com>
2023-11-17 10:20:46 +0100

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,4 @@
 -- Creates  the database hbtn_0d_usa and the table states.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(UNIQUE id INT AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL)
-	PRIMARY KEY(id);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
