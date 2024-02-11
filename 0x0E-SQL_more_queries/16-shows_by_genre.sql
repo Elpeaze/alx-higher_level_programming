a631ecf12f3da3ed0487ef55341ef8844b25b46f
Elpeaze <elpeaze@gmail.com>
2024-02-11 09:59:53 +0100

    second part completed

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,4 @@
---Import the database dump from hbtn_0d_tvshows to your MySQL server
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-
 SELECT title, name FROM tv_genres
 RIGHT JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
